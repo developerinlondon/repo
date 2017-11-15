7c054755896fc74e29adf7075ced8cf7f53f0f65
Nayeem Syed <developerinlondon@gmail.com>
2017-11-15 17:09:39 +0000

    bumped to v0.1.1

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 set -ex
-repo init -u git+ssh://git@bitbucket.com/sdsgmbh/manifest -b refs/tags/v0.1 #  -g base,soe \
+repo init -u git+ssh://git@bitbucket.com/sdsgmbh/manifest -b refs/tags/v0.1.1 #  -g base,soe \
 repo sync --no-clone-bundle

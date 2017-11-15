6bc8f5664bd4bb7c46fe36fad8c073fd768fc313
Nayeem Syed <developerinlondon@gmail.com>
2017-11-15 16:51:32 +0000

    seems we can only use xml

--- init.sh
+++ init.sh
@@ -1,6 +1,2 @@
-./convert.rb -f manifest.yml -m y2x > manifest.xml
-repo init --repo-url git+ssh://git@bitbucket.com/sdsgmbh/android_repo -m ./manifest.yml \
-  -u git+ssh://git@bitbucket.com/sdsgmbh/repo \
-  -b master
-#  -g base,soe \
+repo init -u git+ssh://git@bitbucket.com/sdsgmbh/repo #  -g base,soe \
 repo sync --no-clone-bundle
